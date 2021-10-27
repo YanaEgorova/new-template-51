8f270274e0612db7883112b0ebd4470667a59b1e
Yana <yanaegorova69.3@gmail.com>
2021-10-27 21:01:27 +0300

    +

---
+++ js/data/products.js
@@ -0,0 +1,306 @@
+export const products = [
+    {
+        id: 'product-1',
+        name: 'Wrist Stabilizer',
+        price: 19.99,
+        image: './img/img1.png',
+        description: [
+            {
+                title: 'Breathable Fabric',
+                text: 'Adopting 3D patented breathable fabric, skin-friendly, soft touch, comfortable, lightweight, without perspiration and any restriction. Keeping your injured thumb dry and speed up recovery.',
+            },
+            {
+                title: 'Bendable Splint',
+                text: 'Bendable splint can efficiently stabilize and support weak thumb while offering free movement of other digits. Recovering from injury and convenient for daily activities, texting, golf, cycling, gaming and play digital products like phone.',
+            },
+            {
+                title: 'Perfect Fit With Adjustable Strap',
+                text: 'Two adjustable straps fit for customized compression. Equipped with firm and durable buckle, the thumb brace is versatile, useful and multifunctional. You are allowed to obtain customized fit and maximum stability.',
+            },
+            {
+                title: 'Thumb Pain Relief',
+                text: 'It can stabilize and support your weak or injured thumb joint. Helping you to get relief from aching, sore, swelling caused by arthritis, ligament tears, sprained hand, tendinitis, repetitive stress injury, CMC, Carpel Tunnel Syndrome, and more!',
+            },
+            {
+                title: 'Easy to Use',
+                text: 'It is universal and adjustable. Ergonomic design allows you to wear and take off it easily. It fits for both left and right hands. Really versatile and useful.',
+            },
+        ],
+        images: ['./img/img1-1.png', './img/img1-2.png', './img/img1-3.png', './img/img1-4.png', './img/img1-5.png'],
+    },
+    {
+        id: 'product-2',
+        name: 'Yoga Mat',
+        price: 24.99,
+        image: './img/img2.png',
+        description: [
+            {
+                text: 'Comes with a regular mat and a knee pad. Regular mat measures 71" long 24" wide, 1/2" thick, ensures comfort for people of all shapes and sizes. Knee pad measures 24" wide 10" long, 3/5" thick.',
+            },
+            {
+                text: 'With double sided non-slip surfaces, BalanceFrom all-purpose premium exercise yoga mat comes with an excellent slip resistant advantage to prevent injuries. It also comfortably cushions spine, hips, knees and elbows on hard floors.',
+            },
+            {
+                text: 'Exceptional resilience allow you to keep your balance during any exercise style. Moisture resistant technology makes the mat to be easily washed with soap and water.',
+            },
+        ],
+        images: ['./img/img2-1.png', './img/img2-2.png', './img/img2-3.png', './img/img2-4.png', './img/img2-5.png', './img/img2-6.png', './img/img2-7.png'],
+    },
+    {
+        id: 'product-3',
+        name: 'Hex Dumbbell',
+        price: 35.32,
+        image: './img/img3.png',
+        description: [
+            {
+                text: 'Dumbbells are widely used in gyms and homes for various exercise purposes, a great tool for either full body workout, or specific muscle groups.',
+            },
+            {
+                text: 'High quality solid cast dumbbell encased in rubber, dumbbells will last throughout the years while protect your floor.',
+            },
+            {
+                text: 'Solid metal chrome finish contoured (ergo) handles designed to fit comfortably. Textured handle provides a tight and secure grip.',
+            },
+            {
+                text: 'Hexagon shape rubber encased heads help prevent rolling and provide easier storage.',
+            },
+            {
+                text: 'Dumbbells come in pairs or singles at your choice.',
+            },
+        ],
+        images: ['./img/img3-1.png', './img/img3-2.png', './img/img3-3.png', './img/img3-4.png'],
+    },
+    {
+        id: 'product-4',
+        name: 'Golf Putting Green',
+        price: 39.99,
+        image: './img/img4.png',
+        description: [
+            {
+                text: 'Manufactured from PET resin.',
+            },
+            {
+                text: 'Manufactured in the USA.',
+            },
+            {
+                text: 'Kidney shaped practice putting green.',
+            },
+            {
+                text: 'High quality putting surface stays smooth.',
+            },
+            {
+                text: 'Built in sand trap cutouts catch missed shots.',
+            },
+            {
+                text: '3 practice cup cutouts.',
+            },
+            {
+                text: 'Measures 3 by 9 feet.',
+            },
+            {
+                text: 'Non skid backing.',
+            },
+        ],
+        images: ['./img/img4-1.png', './img/img4-2.png', './img/img4-3.png'],
+    },
+    {
+        id: 'product-5',
+        name: 'Kayak Cover',
+        price: 43.98,
+        image: './img/img5.png',
+        description: [
+            {
+                title: 'Kayak Cover',
+                text: 'Touring deck cover zips on to convert open deck to a 1 person sit-inside deck for added warmth and protection from the elements.',
+            },
+            {
+                title: 'Deck Cover For The Kayak',
+                text: 'This cover easily zips  to convert it into a partially covered - decked boat that helps keep water splashes out.',
+            },
+            {
+                title: 'Sun Shield And Splash Shield',
+                text: 'Our deck cover makes the kayak more of a “sit-in” than a “sit-on-top” because your lower body is covered and protected from sun and splashes without the use of a skirt.',
+            },
+            {
+                title: 'Protection Options',
+                text: 'For even more protection from the elements, the cover makes the boat compatible with a proper spray skirt, allowing you the versatility to add one if you choose.',
+            },
+            {
+                title: 'Aquaglide',
+                text: 'Our passion for water sports has yielded world class aqua parks, inflatable kayaks and stand up paddle boards Designed by adventurers, our products are made for water recreation excellence.',
+            },
+        ],
+        images: ['./img/img5-1.png'],
+    },
+    {
+        id: 'product-6',
+        name: 'Dumbbell Rack',
+        price: 49.95,
+        image: './img/img6.png',
+        description: [
+            {
+                text: 'Holds 200 pounds - Designed to hold 5 pairs of cap hex or rubber hex dumbbells ranging from 5 to 25 pounds; Mix and match your dumbbells to fit this rack, up to 35 pounds pairs; Some combinations may not fit.',
+            },
+            {
+                text: 'Space-saving design - Carbon a frame design of this rack maximizes floor space by storing dumbbells in an “A” shape, or vertical fashion; Easily access and store dumbbells.',
+            },
+            {
+                text: 'Durable and reliable - Finished with a durable powder coat and constructed of steel tubing, this rack will safely hold up to 200 pounds worth of dumbbells.',
+            },
+            {
+                text: 'Specs - Rack weighs 10.5 pounds; 30 day manufacturer’s warranty; Assembly required; Includes rack only.',
+            },
+            {
+                text: 'Item Dimensions - 14.4” length x 12.8” width x 26” height.',
+            },
+            {
+                text: 'Sport type: Weightlifting. Hold neoprene dumbbells.',
+            },
+        ],
+        images: ['./img/img6-1.png','./img/img6-2.png'],
+    },
+    {
+        id: 'product-7',
+        name: 'Home Gym',
+        price: 69.99,
+        image: './img/img7.png',
+        description: [
+            {
+                title: 'Get Sexy Abs In Just 8 Minutes A Day',
+                text: 'This is scientifically designed to combine 8 muscle-burning exercises in 8 minutes a day in one amazingly effective machine.',
+            },
+            {
+                title: 'Dual- Action Resistance',
+                text: 'Power-assisted rebound system provides resistance & support in both directions, maximizing results & minimizing strain.',
+            },
+            {
+                title: 'Customize Your Workout',
+                text: 'Customize your routine with 3 resistance levels. Burn calories & firm up while you\'re sitting down.',
+            },
+        ],
+        images: ['./img/img7-1.png', './img/img7-2.png', './img/img7-3.png'],
+    },
+    {
+        id: 'product-8',
+        name: 'Stand Bag',
+        price: 79.99,
+        image: './img/img8.png',
+        description: [
+            {
+                text: 'Package length: 22.86 cm.',
+            },
+            {
+                text: 'Package width: 30.48 cm.',
+            },
+            {
+                text: 'Package height: 91.44 cm',
+            },
+            {
+                text: 'Product Type: GOLF CLUB BAG.',
+            },
+        ],
+        images: ['./img/img8-1.png', './img/img8-2.png'],
+    },
+    {
+        id: 'product-9',
+        name: 'Handstand Canes',
+        price: 84.41,
+        image: './img/img9.png',
+        description: [
+            {
+                title: 'Collapsible & Adjustable',
+                text: 'Adjustable based canes comes with three parts, one base with a carry handle and two blocks which is attached firmly to metal sticks to elevate your inversions. Height adjustable design. Easy to move and store.',
+            },
+            {
+                title: 'Simple Assemble',
+                text: 'Only simple assemble is required. Just insert two canes into the base board, then you may start exercise right away. Blocks with certain shape and angle can minimize wrist pain.',
+            },
+            {
+                title: 'Large Capacity',
+                text: 'Made of laminated pine(MDF) and stainless steel, durable and comfortable. No lacquer applied so wood maintains it\'s natural wood feel. Capacity up to 200KG/440LBS. Each base is anti-slip for extra stability.',
+            },
+            {
+                title: 'Adjustable Base',
+                text: 'Adjustable based canes are popular model because of its sturdy base, and adjustable shoulder width, For all skill levels from beginner to advanced. and Suitable for all ages from children to Adults.Improve your balance, strengthen your strength, and elevate your shapes. A best balance trainer for home, gym or park.',
+            },
+        ],
+        images: ['./img/img9-1.png', './img/img9-2.png'],
+    },
+    {
+        id: 'product-10',
+        name: 'Batting Tee',
+        price: 89.99,
+        image: './img/img10.png',
+        description: [
+            {
+                text: 'Most Durable, Stable and Portable of any batting tee.',
+            },
+            {
+                text: 'Will not tip over. No outside weight needed. 24" - 46" Stem.',
+            },
+            {
+                text: 'Always-feel-the-ball flexible top.',
+            },
+            {
+                text: 'Unlike similar designed tees, our patented internal interlocking bolt design prevents slippage of the upper tee stem. Patent Pending',
+            },
+        ],
+        images: ['./img/img10-1.png', './img/img10-2.png', './img/img10-3.png'],
+    },
+    {
+        id: 'product-11',
+        name: 'Weight Vest',
+        price: 119.98,
+        image: './img/img11.png',
+        description: [
+            {
+                text: 'Maximum Comfort with flat, flexible weights that move with your body.',
+            },
+            {
+                text: 'Breathable Shell with padded shoulders.',
+            },
+            {
+                text: 'Inspired slim fitting design maximizes mobility.',
+            },
+            {
+                text: 'Road Ready with high visibility reflective materials and media player pocket.',
+            },
+            {
+                text: 'Enclosure ensures athletic fit and function.',
+            },
+        ],
+        images: ['./img/img11-1.png', './img/img11-2.png', './img/img11-3.png', './img/img11-4.png'],
+    },
+    {
+        id: 'product-12',
+        name: 'Cardio Exerciser',
+        price: 148.00,
+        image: './img/img12.png',
+        description: [
+            {
+                title: 'Adjustable Resistance',
+                text: 'This bike is built with a manual resistance knob that allows every user to easily adjust the resistance for a more customized workout. Its magnetic resistance offers smooth transitions between levels for uninterrupted training.',
+            },
+            {
+                title: 'Portable And Compact Equipment',
+                text: 'Strategically engineered with a space-saving design that\'s perfect for any home gym, this pedal machine’s structure is built to give every user the convenience of performing workouts anytime, anywhere, even in the office!',
+            },
+            {
+                title: 'Digital Tracking',
+                text: 'Improved with an easy-to-read computer display that tracks your workout throughout your training session, this mini cycle machine also features a handy scan mode, allowing you to conveniently review your stats and monitor your progress.',
+            },
+            {
+                title: 'Upper- And Lower-Body Workout',
+                text: 'Tone your body and achieve that dream physique without worries! This fitness gear is designed to offer both upper- and lower-body training. The maximum weight capacity of 250 lbs. makes it ideal for various user weights.',
+            },
+            {
+                title: 'Reliable And Secure',
+                text: 'Give yourself a worry-free exercise session. This apparatus features rubberized feet designed to increase stability and prevent the equipment from unnecessary movements. It features grooved pedals for maximum hand and foot grip.',
+            },
+            {
+                text: 'Sport type: Cycling.',
+            },
+        ],
+        images: ['./img/img12-1.png','./img/img12-2.png','./img/img12-3.png'],
+    },
+]
